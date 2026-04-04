c7c0417b6267b4a09b870449a1b3f1f0b56b5ca1
LekaiYao <lekai.yao@cern.ch>
2026-04-04 11:17:58 +0200

    update for PbPb training

--- run.sh
+++ run.sh
@@ -5,13 +5,16 @@ echo "Host: $(hostname)"
 echo "Time: $(date)"
 
 train_tag=$1
+optuna_n_trials=$2
 echo "Train tag: ${train_tag}"
+echo "OPTUNA_N_TRIALS: ${optuna_n_trials}"
 
 repo_dir="/eos/home-l/leyao/pbpb_work/X_analysis/XGBoost"
 cd "${repo_dir}"
 
 source "${repo_dir}/.venv/bin/activate"
 export PYTHONNOUSERSITE=1
+export OPTUNA_N_TRIALS=${optuna_n_trials}
 
 echo "Python: $(which python3)"
 
